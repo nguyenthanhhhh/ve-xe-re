607857b5ddd21b9a3c1bd3694aaed1177b30145d
Nguyen Thanh <nguyenthanhvlcm@gmail.com>
2023-04-11 23:46:02 +0700

    config file index model

--- src/app/models/index.js
+++ src/app/models/index.js
@@ -14,10 +14,14 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
+    process.env.DB_NAME,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: process.env.DB_DIALECT,
+      logging: process.env.LOGGING,
+    }
   );
 }
 
